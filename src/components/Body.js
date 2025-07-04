2e5fafa565e20b47770e95e815641150074d2c31
ishiwangi16 <2022ugee009@nitjsr.ac.in>
2025-07-05 00:47:01 +0530

    Update with new features

--- src/components/Body.js
+++ src/components/Body.js
@@ -15,7 +15,7 @@ const Body = () => {
   const fetchData = async () => {
     try {
       const data = await fetch(
-        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=21.99740&lng=79.00110&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+        "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.776838&lng=86.146225&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
       );
       const json = await data.json();
 
